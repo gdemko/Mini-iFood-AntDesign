13c956e174989718470af84f22b5d1a03a570933
Guilherme <guilherme.demko@outlook.com>
2022-08-29 21:01:38 -0300

    feat: adicionado processo do projeto para executar na porta 8002

--- nuxt.config.js
+++ nuxt.config.js
@@ -80,6 +80,6 @@ export default {
   // Build Configuration (https://go.nuxtjs.dev/config-build)
   build: {},
   server: {
-    port: 8000, // default: 3000
+    port: 8002 // default: 3000
   }
 }
